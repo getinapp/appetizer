66859944fe197e30b652bc4344d7d432d74d29dd
Wendel Freitas <wendelfreitasbranco@gmail.com>
2021-09-01 11:19:31 -0300

    chore: change text color

--- src/components/MyAwesomeComponent/index.tsx
+++ src/components/MyAwesomeComponent/index.tsx
@@ -4,4 +4,6 @@ import React from 'react';
  * My Awesome Component
  */
 
-export const MyAwesomeComponent = () => <h1>Hello Appetizer @getinapp</h1>;
+export const MyAwesomeComponent = () => (
+  <h1 style={{ color: 'red' }}>Hello Appetizer @getinapp</h1>
+);
