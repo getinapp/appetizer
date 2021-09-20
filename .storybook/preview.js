b77eb3404f7da1693592a7817fdcafab98c26786
Wendel Freitas <wendelfreitasbranco@gmail.com>
2021-09-20 11:46:21 -0300

    chore: apply theme provider in stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from '../src/styles/global';
+import theme from '../src/styles/themes/light';
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
