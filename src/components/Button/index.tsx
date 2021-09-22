6d415b90b6d2fdb72e717b94a46a76085b3f14d5
Wendel Freitas <wendelfreitasbranco@gmail.com>
2021-09-22 15:32:41 -0300

    feat: create button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import * as S from './styles';
+
+type ButtonSizesType = 'small' | 'regular' | 'large';
+type ButtonVariantsType =
+  | 'primary'
+  | 'secondary'
+  | 'white'
+  | 'outline'
+  | 'outline-white';
+
+export type ButtonProps = {
+  /**
+   * The content of the component.
+   */
+  children?: React.ReactElement | string;
+  /**
+   * The variant of the button. It supports just two type of variants: 'primary' and 'secondary'.
+   */
+  variant?: ButtonVariantsType;
+  /**
+   * How large should the button be?
+   */
+  size?: ButtonSizesType;
+  /**
+   * In the case of the button, have the maximum size of the container
+   */
+  isFullWidth?: boolean;
+  /**
+   * Optional click handler
+   */
+  onClick?: (event?: React.MouseEvent<HTMLButtonElement>) => void;
+  /**
+   * Disabled state of button
+   */
+  disabled?: boolean;
+  /**
+   * Set the loading status of button
+   */
+  loading?: boolean;
+};
+
+/**
+ * API documentation for the React Button component. Learn about the available props, and the CSS API.
+ */
+export const Button = ({
+  children,
+  variant = 'primary',
+  size = 'regular',
+  isFullWidth = false,
+  disabled = false,
+  onClick,
+}: ButtonProps) => {
+  return (
+    <S.Container
+      variant={variant}
+      size={size}
+      isFullWidth={isFullWidth}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <span>{children}</span>
+    </S.Container>
+  );
+};
