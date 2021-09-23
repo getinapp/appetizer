a4905fd5b865c2b41068dd8f6b95364cdf955ff4
Wendel Freitas <wendelfreitasbranco@gmail.com>
2021-09-23 14:45:50 -0300

    feat: create paragraph typography component

---
+++ src/components/Paragraph/Paragraph.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import Paragraph from '.';
+
+describe('<Paragraph />', () => {
+  it('should render the paragraph with the default size', () => {
+    renderWithTheme(<Paragraph>Testing Paragraph</Paragraph>);
+
+    expect(screen.getByText(/Testing Paragraph/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/Testing Paragraph/i)).toHaveStyle({
+      'font-size': '1.4rem',
+    });
+  });
+
+  it('should render the paragraph with the lead size', () => {
+    renderWithTheme(<Paragraph type="lead">Testing Paragraph</Paragraph>);
+
+    expect(screen.getByText(/Testing Paragraph/i)).toBeInTheDocument();
+
+    expect(screen.getByText(/Testing Paragraph/i)).toHaveStyle({
+      'font-size': '1.8rem',
+    });
+  });
+});
