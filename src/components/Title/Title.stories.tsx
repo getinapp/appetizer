9ef3cb5d92eb6286a45415d169130b77a9343852
Wendel Freitas <wendelfreitasbranco@gmail.com>
2021-09-23 14:46:10 -0300

    feat: create title typography component

---
+++ src/components/Title/Title.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import Title, { TitleProps } from '.';
+
+export default {
+  title: 'Typography/Title',
+  component: Title,
+  argTypes: {
+    icon: {
+      type: '',
+    },
+    children: {
+      type: 'string',
+    },
+  },
+} as Meta;
+
+export const Default: Story<TitleProps> = (args) => <Title {...args} />;
+
+Default.args = {
+  children: 'Hello Get In',
+};
