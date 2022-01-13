84cf28ecb0e12bb4c2db999904cbb06981140455
Wendel Freitas <wendelfreitasbranco@gmail.com>
2022-01-13 20:14:03 -0300

    chore: add prefix into story title

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -4,7 +4,7 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 import { ButtonProps, Button } from '.';
 
 export default {
-  title: 'Button',
+  title: 'Components/Button',
   component: Button,
   argTypes: {
     variant: {
