f7eeb2fd5c70148bcc08336ab4878aaa2108fc6b
Wendel Freitas <wendelfreitasbranco@gmail.com>
2022-01-14 13:51:05 -0300

    feat: add react-modal package

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -54,6 +54,30 @@ const GlobalStyles = createGlobalStyle`
     }
   `}
 
+  .ReactModal__Overlay {
+      opacity: 0;
+      transition: opacity 500ms ease-in-out;
+    }
+
+    .ReactModal__Overlay--after-open {
+      opacity: 1;
+    }
+
+    .ReactModal__Overlay--before-close {
+      opacity: 0;
+    }
+  }
+
+  .modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+  }
+
+
 `;
 
 export default GlobalStyles;
