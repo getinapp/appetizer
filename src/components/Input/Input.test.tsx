e6998142a836bd8011ffe2eb6e9020072ff9e244
Wendel Freitas <wendelfreitasbranco@gmail.com>
2022-01-16 21:21:22 -0300

    feat: add prop to set icon and mask

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -4,17 +4,18 @@ import userEvent from '@testing-library/user-event';
 
 import { Input } from '.';
 import { renderWithTheme } from 'utils/tests/helpers';
+import { PersonAdd } from '@styled-icons/ionicons-solid/PersonAdd';
 
 describe('<Input />', () => {
   it('renders component successfully', () => {
-    renderWithTheme(<Input title="Nome completo" />);
+    renderWithTheme(<Input label="Nome completo" />);
     const text = screen.getByText('Nome completo');
 
     expect(text).toBeInTheDocument();
   });
 
   it('should be render input with error message', () => {
-    renderWithTheme(<Input title="Name" error="Name required" />);
+    renderWithTheme(<Input label="Name" error="Name required" />);
     const text = screen.getByText('Name');
     const error = screen.getByText('Name required');
 
@@ -23,7 +24,7 @@ describe('<Input />', () => {
   });
 
   it('should be render input with helper message', () => {
-    renderWithTheme(<Input title="Name" helper="Helper text" />);
+    renderWithTheme(<Input label="Name" helper="Helper text" />);
     const text = screen.getByText('Name');
     const helper = screen.getByText('Helper text');
 
@@ -32,7 +33,7 @@ describe('<Input />', () => {
   });
 
   it('should be change label color when have error', () => {
-    renderWithTheme(<Input title="Name" error="Name required" />);
+    renderWithTheme(<Input label="Name" error="Name required" />);
     const text = screen.getByText('Name');
 
     const input = screen.getByRole('textbox', { name: 'Name' });
@@ -45,7 +46,7 @@ describe('<Input />', () => {
   });
 
   it('should be change helper color when have helper text', () => {
-    renderWithTheme(<Input title="Name" helper="Helper text" />);
+    renderWithTheme(<Input label="Name" helper="Helper text" />);
     const text = screen.getByText('Helper text');
 
     const input = screen.getByRole('textbox', { name: 'Name' });
@@ -59,22 +60,19 @@ describe('<Input />', () => {
 
   it('input should be focussed when user press', async () => {
     const spyOnFocus = jest.fn();
-    renderWithTheme(<Input title="Name" onFocus={spyOnFocus} />);
+    const { container } = renderWithTheme(
+      <Input label="Name" onFocus={spyOnFocus} />,
+    );
+    const input = container.getElementsByTagName('input')[0];
 
-    const text = screen.getByRole('textbox', { name: 'Name' });
-
-    fireEvent.focus(text);
+    fireEvent.focus(input);
 
     await waitFor(() => expect(spyOnFocus).toHaveBeenCalled());
-
-    expect(text).toHaveStyle({
-      border: '1px solid black',
-    });
   });
 
   it('input should be not call function when not pass onFocus', async () => {
     const spyOnFocus = jest.fn();
-    renderWithTheme(<Input title="Name" />);
+    renderWithTheme(<Input label="Name" />);
 
     const text = screen.getByRole('textbox', { name: 'Name' });
 
@@ -85,7 +83,7 @@ describe('<Input />', () => {
 
   it('input should be not focussed when user press onBlur', async () => {
     const spyOnFocus = jest.fn();
-    renderWithTheme(<Input title="Name" onBlur={spyOnFocus} />);
+    renderWithTheme(<Input label="Name" onBlur={spyOnFocus} />);
 
     const text = screen.getByRole('textbox', { name: 'Name' });
 
@@ -96,7 +94,7 @@ describe('<Input />', () => {
 
   it('input should be not call function when not pass onBlur', async () => {
     const spyOnFocus = jest.fn();
-    renderWithTheme(<Input title="Name" />);
+    renderWithTheme(<Input label="Name" />);
 
     const text = screen.getByRole('textbox', { name: 'Name' });
 
@@ -106,12 +104,55 @@ describe('<Input />', () => {
   });
 
   it('should be accessible by tab', () => {
-    renderWithTheme(<Input title="Name" name="test" />);
+    renderWithTheme(<Input label="Name" name="test" />);
 
     const input = screen.getByRole('textbox', { name: 'Name' });
     expect(document.body).toHaveFocus();
 
     userEvent.tab();
     expect(input).toHaveFocus();
+  });
+
+  it('should renders with Icon', () => {
+    renderWithTheme(<Input icon={<PersonAdd data-testid="icon" />} />);
+
+    expect(screen.getByTestId('icon')).toBeInTheDocument();
+  });
+
+  it('should renders with Icon on the right side', () => {
+    renderWithTheme(
+      <Input icon={<PersonAdd data-testid="icon" />} iconPosition="right" />,
+    );
+
+    expect(screen.getByTestId('icon').parentElement).toHaveStyle({ order: 1 });
+  });
+
+  it('should render label with margin left if have icon', async () => {
+    renderWithTheme(
+      <Input label="Name" icon={<PersonAdd data-testid="icon" />} />,
+    );
+    const input = screen.getByRole('textbox', { name: 'Name' });
+
+    const text = screen.getByText('Name');
+
+    fireEvent.focus(input);
+
+    expect(text).toHaveStyle({ marginLeft: '1.5rem' });
+    expect(input).toHaveStyle({ width: '100%' });
+  });
+
+  it('should render input with max width', async () => {
+    renderWithTheme(
+      <Input
+        label="Name"
+        icon={<PersonAdd data-testid="icon" />}
+        iconPosition="right"
+      />,
+    );
+    const input = screen.getByRole('textbox', { name: 'Name' });
+
+    fireEvent.focus(input);
+
+    expect(input).toHaveStyle({ width: '100%' });
   });
 });
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,16 @@
 import styled, { css } from 'styled-components';
+import { IMaskInput } from 'react-imask';
+
 import { InputProps } from '.';
+
+type StyleInputProps = {
+  active: boolean;
+  error: boolean;
+  required: boolean;
+  icon: boolean;
+  disabled: boolean;
+  iconPosition: 'right' | 'left';
+};
 
 export const Container = styled.div``;
 
@@ -9,53 +20,117 @@ export const InputContainer = styled.div`
   justify-content: center;
   position: relative;
   font-size: inherit;
+  margin: 0.75rem 0;
 `;
 
-export const Label = styled.label<{ active: boolean; error: boolean }>`
+export const Label = styled.label<Omit<StyleInputProps, 'disabled'>>`
   position: absolute;
-  padding: 0 0.35rem;
+  padding: ${({ active }) => (active ? '0 0.4rem' : '0')};
 
-  ${({ theme, error, active }) => css`
-    color: ${error && active ? theme.colors.error : theme.colors.black};
+  ${({ theme, error, active, required }) => css`
+    color: ${!!error && active ? theme.colors.error : theme.colors.black};
+    font-size: ${active ? theme.font.sizes.xsmall : theme.font.sizes.normal};
+    border-radius: ${theme.border.radius.large};
+
+    ${required &&
+    css`
+      &:before {
+        content: '* ';
+        color: ${theme.colors.primary};
+      }
+    `}
   `}
   transition: all 0.2s ease-in-out;
   transform-origin: left top;
   cursor: text;
   pointer-events: none;
   transform: ${({ active }) =>
-    active ? 'translate3d(0, -92.5%, 0) scale(0.9)' : 'none'};
+    active ? 'translate3d(0, -110%, 0) scale(1)' : 'none'};
 
   background: white;
-  margin-left: 15px;
-  font-size: 1.3rem;
+
+  margin-left: ${({ active, icon, iconPosition }) =>
+    active ? '1.5rem' : icon && iconPosition === 'left' ? '4.1rem' : '1.5rem'};
 `;
 
-export const Input = styled.input<InputProps>`
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  border-radius: 8px;
-  padding: 0.7rem 1rem;
-  padding-top: 1rem;
-  padding-left: 1.3rem;
-  outline: none;
-
-  ${({ theme, active, error }) => css`
-    font-family: ${theme.font.family};
-    font-style: normal;
-    font-weight: normal;
-    font-size: 1.25rem;
-    border: 1px solid ${active ? 'black' : theme.colors.placeholder};
-
+export const InputWrapper = styled.div<
+  Pick<
+    StyleInputProps,
+    'active' | 'error' | 'disabled' | 'icon' | 'iconPosition'
+  >
+>`
+  ${({ theme, icon, iconPosition, active, error, disabled }) => css`
+    display: flex;
+    align-items: center;
+    border: 1px solid ${active ? theme.colors.black : theme.colors.placeholder};
+    border-radius: ${theme.border.radius.large};
     ${error &&
     css`
       border: 1px solid ${theme.colors.error};
     `}
 
+    ${disabled &&
+    css`
+      border: 1px solid ${theme.colors.disabled};
+
+      background: ${theme.colors.lowContrast};
+      svg {
+        color: ${theme.colors.placeholder};
+      }
+    `}
+
+    > input {
+      padding-left: 1.5rem;
+
+      ${icon &&
+      css`
+        padding-left: ${iconPosition === 'right' ? '1.75rem' : '1rem'};
+      `}
+    }
+  `}
+`;
+
+export const Input = styled(IMaskInput)<InputProps>`
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0.7rem 1rem;
+  padding-top: 1rem;
+  padding-left: 1.3rem;
+  outline: none;
+
+  ${({ theme, disabled }) => css`
+    font-family: ${theme.font.family};
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.5rem;
+    border: none;
+    width: 100%;
+    border-radius: ${theme.border.radius.large};
+
+    ${disabled &&
+    css`
+      &:hover {
+        cursor: not-allowed;
+      }
+    `}
+
     &::placeholder {
       color: ${theme.colors.placeholder};
-      opacity: ${active ? 1 : 0};
       transition: opacity 0.2s cubic-bezier(0.6, 0.04, 0.98, 0.335);
+    }
+  `}
+`;
+
+export const Icon = styled.div<{ iconPosition?: 'left' | 'right' }>`
+  ${({ theme, iconPosition }) => css`
+    display: flex;
+    color: ${theme.colors.black};
+    order: ${iconPosition === 'right' ? 1 : 0};
+    & > svg {
+      height: 100%;
+      margin-left: 1.5rem;
+      margin-right: ${iconPosition === 'right' ? '1.5rem' : 0};
     }
   `}
 `;
@@ -63,10 +138,9 @@ export const Input = styled.input<InputProps>`
 export const Error = styled.p`
   font-weight: normal;
   margin-left: 0.5rem;
-  margin-top: 0.5rem;
-  font-size: 1.05rem;
 
   ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xsmall};
     color: ${theme.colors.error};
   `}
 `;
@@ -74,10 +148,9 @@ export const Error = styled.p`
 export const Helper = styled.p<{ active: boolean }>`
   font-weight: normal;
   margin-left: 0.5rem;
-  margin-top: 0.5rem;
-  font-size: 1.1rem;
 
   ${({ theme, active }) => css`
+    font-size: ${theme.font.sizes.xsmall};
     color: ${active ? theme.colors.black : theme.colors.buttonOutlineBorder};
   `}
 `;
