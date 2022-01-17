8eab1598c66a6f44cfa471e8b9974365cfde5939
Wendel Freitas <wendelfreitasbranco@gmail.com>
2022-01-16 21:21:22 -0300

    feat: add prop to set icon and mask

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { PersonAdd } from '@styled-icons/ionicons-solid/PersonAdd';
 
 import { InputProps, Input } from '.';
 
@@ -7,7 +8,7 @@ export default {
   title: 'Forms/Input',
   component: Input,
   argTypes: {
-    title: {
+    label: {
       control: {
         type: 'text',
       },
@@ -32,8 +33,22 @@ export const Default: Story<InputProps> = (args) => (
 );
 
 Default.args = {
-  title: 'Full name',
+  label: 'Full name',
   placeholder: 'Testing',
+  required: true,
+};
+
+export const Disabled: Story<InputProps> = (args) => (
+  <div style={{ marginTop: 20 }}>
+    <Input {...args} />
+  </div>
+);
+
+Disabled.args = {
+  label: 'Full name',
+  placeholder: 'Testing',
+  required: true,
+  disabled: true,
 };
 
 export const withError: Story<InputProps> = (args) => (
@@ -43,7 +58,7 @@ export const withError: Story<InputProps> = (args) => (
 );
 
 withError.args = {
-  title: 'Full name',
+  label: 'Full name',
   placeholder: 'Testing',
   error: 'Error text',
 };
@@ -55,7 +70,7 @@ export const withHelper: Story<InputProps> = (args) => (
 );
 
 withHelper.args = {
-  title: 'Full name',
+  label: 'Full name',
   placeholder: 'Testing',
   helper: 'This is a helper text',
 };
@@ -67,5 +82,60 @@ export const withoutPlaceholder: Story<InputProps> = (args) => (
 );
 
 withoutPlaceholder.args = {
-  title: 'Address',
+  label: 'Address',
+};
+
+export const withIcon: Story<InputProps> = (args) => (
+  <div>
+    <Input {...args} />
+    <Input {...args} placeholder="teste" />
+  </div>
+);
+
+withIcon.args = {
+  label: 'Username',
+  icon: <PersonAdd size={15} />,
+};
+
+export const withCurrencyMask: Story<InputProps> = (args) => (
+  <div style={{ marginTop: 20 }}>
+    <Input
+      {...args}
+      placeholder="R$ 0,00"
+      mask="R$ price"
+      blocks={{
+        price: {
+          mask: Number,
+          padFractionalZeros: true,
+          thousandsSeparator: '.',
+          radix: ',',
+          mapToRadix: ['.'],
+        },
+      }}
+    />
+  </div>
+);
+
+withCurrencyMask.args = {
+  label: 'Currency',
+};
+
+export const withCpfCnpjMask: Story<InputProps> = (args) => (
+  <div style={{ marginTop: 20 }}>
+    <Input
+      {...args}
+      mask={[
+        {
+          mask: '000.000.000-00',
+        },
+        {
+          mask: '00.000.000/0000-00',
+        },
+      ]}
+    />
+  </div>
+);
+
+withCpfCnpjMask.args = {
+  label: 'CPF/CNPJ',
 };
