842864dcfc2c66a31f594f09ccd761a06a53bf35
Wendel Freitas <wendelfreitasbranco@gmail.com>
2022-01-16 23:56:30 -0300

    feat: build appetizer using rollup with new config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import packageJson from './package.json';
 
@@ -25,14 +23,8 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      terser(),
+      typescript({ useTsconfigDeclarationDir: true }),
     ],
     external: ['react', 'react-dom', 'styled-components'],
-  },
-  {
-    input: 'dist/esm/types/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
   },
 ];
